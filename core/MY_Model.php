523119d72a010202a5325fe1647376cc8a1a11fa
AllanJeremy <aj.dev254@gmail.com>
2019-10-15 09:58:29 +0300

    Initial commit

---
+++ core/MY_Model.php
@@ -0,0 +1,174 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+//Model override
+class MY_Model extends CI_Model
+{
+    protected $table_name;
+
+	function __construct($table_name='')
+	{
+        $this->table_name = $table_name;
+		$this->load->database();
+		// $this->db->save_queries = FALSE;
+	}
+
+	// Generates a random 64 bit string
+	private function _generate_random_string($prepend='',$append='',$length=NULL)
+	{
+		$length = empty($length) ? ID_STRING_LENGTH : $length;
+	
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
+		$characters_length = strlen($characters);
+		$randomString = isset($prepend) ? $prepend : '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $characters_length - 1)];
+		}
+		return $randomString.@$append;
+	}
+
+	/** Get a table field  
+	 * @param string $table_name The name of the table the column/field belongs to
+	 * @param string $field_name The column/field name we want to get a reference to. Defaults to all columns in the specified table
+	 * @return string Returns a complete sql based concatenation of the table and field/column name 
+	*/
+	protected function get_table_field(string $table_name,string $field_name='*'):string
+	{
+		return $table_name.'.'.$field_name;
+	}
+
+	// Checks if an id exists, returns true if it does and false if it doesn't
+	private function _id_exists($table_name,$id_column,$generated_id)
+	{
+		$_field_id = $this->get_table_field($table_name,$id_column);
+		$this->db->select($_field_id);
+
+		// Check if the id exists in the table we are trying to add it to
+		$this->db->where(
+			array(
+				$_field_id => $generated_id
+			)
+		);
+		$item_found = $this->db->get($table_name)->row_object();
+
+		return !empty($item_found);
+	}
+
+	/** Generate a random string id for the table specified 
+	 * @param string $table_name The name of the table we want to check for the id
+	 * @param mixed $id_column Name of the id column we are going to be checking
+	*/
+	public function generate_string_id($table_name,$id_column='id')
+    {		
+		$generated_id = $this->_generate_random_string();
+		
+		$item_exists = $this->_id_exists($table_name,$id_column,$generated_id);
+
+		// If id exists ~ try genrating again (recursively)
+		if($item_exists)
+		{//! Big O of log(N) ~ Consider finding optimization, possibly use dynamic programming
+			$generated_id = $this->generate_string_id($table_name,$id_column);
+		}
+		else
+		{
+			return $generated_id;
+		}
+	}
+
+	/**  Loops through multiple filters and generates relevant search filters 
+	 * @description Using this to allow for duplicate search query entries, eg. Using one search string to get multiple items
+	*/
+	protected function use_search_filters($search_filters)
+	{
+		if(empty($search_filters))
+		{
+			return;
+		}
+		
+		// Only use search filters if they were found
+		for($i=0; $i<count($search_filters); $i++)
+		{
+			$current_filter = $search_filters[$i];
+
+			// This allows for starting the query with a 'AND LIKE' which in turn allows for correct searching 
+			if($i === 0)
+			{
+				$this->db->like($current_filter);
+			}
+			else
+			{
+				$this->db->or_like($current_filter);
+			}
+		}
+		// var_dump($this->db->get_compiled_select());
+	}
+
+	// Uses filter if the limit is not empty
+	protected function use_filters($filters)
+	{
+		if(!empty($filters))
+		{
+			$this->db->where($filters);
+		}
+	}
+
+	// Uses limit if the limit is not empty
+	protected function use_limit($limit,$offset=0)
+	{
+		if(!empty($limit))
+		{
+			$this->db->limit($limit,$offset);
+		}
+    }
+    
+    /* 
+        CRUD
+    */
+    // Determine whether `WHERE` or `LIKE` should be used
+    private function _get_filter_query($filters,$is_strict) # Facade pattern in use
+    {
+        // Use `WHERE` for strict matching
+        if($is_strict)
+        {
+            $this->use_filters($filters);
+        }
+        else // Use `LIKE` for loose matching
+        {
+            $this->use_search_filters($filters);
+        }
+    }
+
+    // Create
+    protected function create($data)
+    {
+        return $this->db->insert($this->table_name,$data);
+    }
+
+    // Insert batch ~ multiple records at once
+    protected function create_batch($data)
+    {
+        return $this->db->insert_batch($this->table_name,$data);
+    }
+
+    // Read
+    protected function read($filters,$limit=NULL,$offset=0,$is_strict=TRUE)
+    {
+        $this->_get_filter_query($fitlers,$is_strict);
+        $this->use_limit($limit,$offset);
+
+        return $this->db->get($this->table_name);
+    }
+
+    // Update ~ optionally use `LIKE`, or `WHERE` ~ defaults to `WHERE`
+    protected function update($filter,$data,$is_strict=TRUE)
+    {
+        $this->_get_filter_query($fitlers,$is_strict);
+        return $this->db->update($this->table_name,$data);
+    }
+
+    // Delete
+    protected function delete($filter,$is_strict=TRUE)
+    {
+        $this->_get_filter_query($fitlers,$is_strict);
+        return $this->db->delete($this->table_name);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,57 @@
+# About
+
+The model override allows for simple creation of models in Codeigniter 3.x
+For default functionality, all you need to do is extend the `MY_Model` class.
+
+In most cases, this means all you have to do is specify the table you want the model to be used with and you are as good as done
+
+## Assumptions
+
+- A single model will only attach to a single table. 
+  
+> Although you may change the value of the table from method to method through `$this->table_name = 'new_table_name'`, this is not recommended as it may be confusing to understand in future.
+
+## Usage
+
+Extend the MY_Model class when creating a model.
+
+```php
+    class Custom_Model extends MY_Model
+    {
+        function __construct()
+        {
+            # This is the table name the model will use
+            parent:__construct('your_table_name');
+        }
+
+        // Overrides go here
+    }
+```
+
+### Using joins
+
+When using joins, simply create an override for the `read` method and add the joins at the top as shown in this example. Ensure that the parameters are the same as the `parent` class(`MY_Model`) method parameters
+
+```php
+    // Public because we won't be extending this class
+    public function read($filters,$limit=NULL,$offset=0,$is_strict=TRUE)
+    {
+        # Your joins go here.. For example:
+        $this->db->join('comments', 'comments.id = blogs.id');
+
+        return $this->read($filters,$limit,$offset,$is_strict);
+    }
+```
+
+## Documentation
+
+I'll be adding some soon
+
+## Contributing
+
+Clone the project, make changes and make a pull request
+Ensure you don't make breaking changes. Any breaking changes should be marked as such while submitting a pull request. Failure to do so will have your request disqualified.
+
+## Bugs & Issues
+
+Feel free to use [github issues](https://www.github.com/AllanJeremy/ci-model) to post any issues you may find. Alternatively, [shoot me a message](mailto:dev@allanjeremy.com)
